34713380b6af24cc36b1a784bd311ee67592892c
tea.t <souri@kth.se>
2019-09-02 19:06:50 +0200

    I don't know why I'm commiting this.

--- scarystuff.js
+++ scarystuff.js
@@ -13,10 +13,14 @@ var kostnadSliderPreview = document.getElementById('kostnad-preview');
 var arbetsgivarSpan = document.getElementById('arbetsgivar');
 var kommunalSpan = document.getElementById('kommunal');
 var jobbskattSpan = document.getElementById('jobbskatt');
+var totallonSpan = document.getElementById('totallon');
+var nettoSpan = document.getElementById('netto');
 
 var rdValue = 0;
 var bruttoValue = 42000;
 var jobbskatt= 2500;
+var bilValue = 0;
+var pensionValue = 0;
 
 //testa att JS kommunicerar
 
@@ -37,7 +41,7 @@ timArvode.noUiSlider.on('update', function (values, handle) {
     timArvodeSliderPreview.innerHTML = values[handle];
 });
 
-// Antal timmar
+// Antal timmar-slider
 noUiSlider.create(antalTimmar, {
   start: [1750],
   step: 5,
@@ -46,11 +50,13 @@ noUiSlider.create(antalTimmar, {
       'max': [2500]
   }
 });
+
+// Värdet på antal timmar
 antalTimmar.noUiSlider.on('update', function (values, handle) {
   antalTimmarSliderPreview.innerHTML = values[handle];
 });
 
-// Bruttolön
+// Bruttolön-slider
 noUiSlider.create(brutto, {
   start: [42000],
   step: 1000,
@@ -59,22 +65,29 @@ noUiSlider.create(brutto, {
       'max': [70000]
   }
 });
+
+// Värdet på Bruttolön
 brutto.noUiSlider.on('update', function (values, handle) {
   bruttoSliderPreview.innerHTML = values[handle];
   bruttoValue = values[handle];
 
-// Uträkning av Kommunalskatt från bruttolönen
+// Visa kommunalskatt från bruttolönen
 displayKommunalChange(values[handle]);
 
-// Uträkning av Arbetsgivaravgift från bruttolönen och RD-värdet
+// Visa arbetsgivaravgift från bruttolönen och RD-värdet
 displayArbetsgivarChange(values[handle], rdValue ? rdValue : 0);
+
+// Visa den totala lönekostnaden
+
+// visa nettokostnad från bruttolön, kommunalskatt och jobbskatteavdrag
+displayNetto(values[handle], jobbskatt);
 });
 
 // Visa den fasta jobbskatteavdraget
 displayJobbskatt(jobbskatt);
 
 
-// Kostnader
+// Kostnad-slider
 noUiSlider.create(kostnad, {
   start: [5000],
   step: 5,
@@ -83,6 +96,8 @@ noUiSlider.create(kostnad, {
       'max': [15000]
   }
 });
+
+// Värdet på kostnaden
 kostnad.noUiSlider.on('update', function (values, handle) {
   kostnadSliderPreview.innerHTML = values[handle];
 });
@@ -103,5 +118,22 @@ function displayArbetsgivarChange(brutto, rd) {
 function displayJobbskatt(jobbskatt) {
   jobbskattSpan.innerHTML = jobbskatt;
 }
+
+function handleBilChange(element) { //Not sure if right
+  bilValue = +element.value;
+}
+
+function handlePensionChange(element) { //Not sure if right
+  pensionValue = +element.value;
+}
+
+function displayTotallon(brutto, bil, ) {
+//TODO
+}
+
+function displayNetto(brutto, jobbskatt){
+nettoSpan.innerHTML = (brutto - (brutto * 0.295) + jobbskatt)
+}
+
 
 
