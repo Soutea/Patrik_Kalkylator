7880b2d07ddf516137aa9f718dfe187835f81edb
tea.t <souri@kth.se>
2019-09-03 09:50:53 +0200

    Yay!

--- scarystuff.js
+++ scarystuff.js
@@ -4,11 +4,13 @@ var timArvode = document.getElementById("myRange1");
 var antalTimmar = document.getElementById("myRange2");
 var brutto = document.getElementById("myRange3");
 var kostnad = document.getElementById("myRange4");
+var utdelning = document.getElementById("myRange5");
 
 var timArvodeSliderPreview = document.getElementById("timarvode-preview");
 var antalTimmarSliderPreview = document.getElementById("antaltimmar-preview");
 var bruttoSliderPreview = document.getElementById("brutto-preview");
 var kostnadSliderPreview = document.getElementById("kostnad-preview");
+var utdelningSliderPreview = document.getElementById("utdelning-preview");
 
 var data = {
   // Inputs
@@ -24,6 +26,8 @@ var data = {
   pension: 0,
   rd: 0,
 
+  utdelning: 273000,
+
   // Calculated values
   kommunal: 12390,
   arbetsgivar: 8996.4,
@@ -32,15 +36,15 @@ var data = {
   skatt: 83547.11,
   nettoUtdelning: 218400,
 
-  // Output values TODO FROM EXCEL
-  omsattning: 0,
-  personal: 0,
-  kostnader: 0,
-  vinst: 0,
-  utdelningsmojlighet: 0,
-  nettoManad: 0,
-  motsvarandeLon: 0,
-  overskott: 0
+  // Output values
+  omsattning: 1225000,
+  personal: 700490.4,
+  kostnader: 144750,
+  vinst: 397759.6,
+  utdelningsmojlighet: 273000,
+  nettoManad: 50310,
+  motsvarandeLon: 87105.14,
+  overskott: 23212.49
 };
 
 // #region
@@ -48,7 +52,8 @@ var sliders = [
   { element: timArvode, start: 750, step: 5, min: 500, max: 1200 },
   { element: antalTimmar, start: 1750, step: 5, min: 0, max: 2500 },
   { element: brutto, start: 42000, step: 1000, min: 30000, max: 70000 },
-  { element: kostnad, start: 5000, step: 5, min: 0, max: 15000 }
+  { element: kostnad, start: 5000, step: 5, min: 0, max: 15000 },
+  { element: utdelning, start: 273000, step: 5, min: 0, max: 1000000 }
 ];
 
 function initSlider(sliderConfig) {
@@ -69,10 +74,9 @@ sliders.forEach(slider => {
 // #endregion
 
 //testa att JS kommunicerar
-
 console.log("hello World!;");
 
-// Timarvode
+// Värdet av timarvode
 timArvode.noUiSlider.on("update", function(values, handle) {
   timArvodeSliderPreview.innerHTML = values[handle];
   data.timArvode = +values[handle];
@@ -86,7 +90,7 @@ antalTimmar.noUiSlider.on("update", function(values, handle) {
   updateValues();
 });
 
-// Värdet på Bruttolön
+// Värdet på bruttolön
 brutto.noUiSlider.on("update", function(values, handle) {
   bruttoSliderPreview.innerHTML = values[handle];
   data.brutto = +values[handle];
@@ -101,6 +105,14 @@ kostnad.noUiSlider.on("update", function(values, handle) {
   updateValues();
 });
 
+// Värdet på utdelningen
+utdelning.noUiSlider.on("update", function(values, handle) {
+  utdelningSliderPreview.innerHTML = values[handle];
+  data.utdelning = +values[handle];
+  updateValues();
+});
+
+// Clickrutor
 function handleRDChange(element) {
   data.rd = element.checked ? 0.1 : 0;
   updateValues();
@@ -117,29 +129,30 @@ function handlePensionChange(element) {
   updateValues();
 }
 
+//Uträkningar
 function updateValues() {
   data.kommunal = data.brutto * 0.295;
   data.arbetsgivar = data.brutto * 0.3142 - data.rd * data.brutto;
   data.nettoLon = data.brutto - data.kommunal + data.jobbskatt;
   data.total = data.brutto + data.arbetsgivar + data.bil1 * (0.3142 + 0.295) + data.pension * 0.3114;
 
-  //TODO CALCULATIONS
-  data.skatt = 0;
-  data.nettoUtdelning = 0;
+  data.skatt = data.vinst * 0.22;
+  data.nettoUtdelning = data.utdelning - data.utdelning * 0.2;
 
-  data.omsattning;
-  data.personal;
-  data.kostnader;
-  data.vinst;
-  data.utdelningsmojlighet;
-  data.nettoManad;
-  data.motsvarandeLon;
-  data.overskott;
+  data.omsattning = data.timArvode * data.antalTimmar;
+  data.personal = data.total * 12 + data.pension * 12;
+  data.kostnader = data.kostnad * 12 + data.bil2 * 12 + 0.03 * data.omsattning;
+  data.vinst = data.omsattning - data.personal - data.kostnader;
+  data.utdelningsmojlighet = data.brutto * 6 + data.bil1 * 6;
+  data.nettoManad = data.nettoUtdelning / 12 + data.nettoLon;
+  data.motsvarandeLon = (data.nettoManad - 32000) * 1.199 + data.nettoManad * 1.295;
+  data.overskott = data.vinst - data.skatt - data.utdelning;
 
   console.log(data);
   updateView();
 }
 
+//Uppdatera vyn
 function updateView() {
   updateDomElement("omsattning", data.omsattning);
   updateDomElement("personal", data.personal);
@@ -150,7 +163,6 @@ function updateView() {
   updateDomElement("motsvarandeLon", data.motsvarandeLon);
   updateDomElement("overskott", data.overskott);
 }
-
 function updateDomElement(elementId, value) {
   document.getElementById(elementId).innerHTML = value;
 }
